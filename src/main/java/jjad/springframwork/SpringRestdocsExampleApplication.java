654fad991ac6d9b01da6d7c1b142dc5cfb6fb967
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-07 09:57:08 -0600

    init commit

---
+++ src/main/java/jjad/springframwork/SpringRestdocsExampleApplication.java
@@ -0,0 +1,13 @@
+package jjad.springframwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestdocsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestdocsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjad/springframwork/SpringRestdocsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jjad.springframwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestdocsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
